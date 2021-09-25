dc1aa47f69a2216792b585352de58616f2fefee5
salahuddinemon <emonthex@gmail.com>
2021-09-26 01:05:31 +0600

    second commint

---
+++ src/fakedb/fakedb.js
@@ -0,0 +1,91 @@
+const teachers = [
+    {
+        key: 'CzqkT9dvAk',
+        img: 'https://i.postimg.cc/0NdrYwDm/teacher2.jpg',
+        name: 'Tamara Vasquez',
+        gender: 'Female',
+        position: 'Asst. Teacher',
+        schoolName: 'Willow Academy',
+        age: '31',
+        area: 'Hempholme',
+        Salary: '$3500'
+    },
+    {
+        key: 'GN0uDpSqKU',
+        img: 'https://i.postimg.cc/gkXJM53G/teacher3.jpg',
+        name: 'Ritthy Burton',
+        gender: 'Male',
+        position: 'Principle Teacher',
+        schoolName: 'Mountainview Middle School',
+        age: '32',
+        area: 'Gramsby',
+        Salary: '$4000'
+    },
+    {
+        key: '8OsQmaDO8N',
+        img: 'https://i.postimg.cc/jdpjBXhP/teacher4.jpg',
+        name: 'Glenda Watts',
+        gender: 'Female',
+        position: 'Teacher',
+        schoolName: 'Cape Coral Charter School',
+        age: '29',
+        area: 'Firebend',
+        Salary: '$3300'
+    },
+    {
+        key: '03tVvZsA9M',
+        img: 'https://i.postimg.cc/rmwy5Y8X/teacher6.jpg',
+        name: 'Ron Scott',
+        gender: 'Male',
+        position: 'Asst. Teacher',
+        schoolName: 'Silver Valley School for Boys',
+        age: '34',
+        area: 'Orilon',
+        Salary: '$3700'
+    },
+    {
+        key: 'ZX8buOWZEV',
+        img: 'https://i.postimg.cc/WzD2HFF5/teacher7.jpg',
+        name: 'Ema Walton',
+        gender: 'Female',
+        position: 'Asst. Teacher',
+        schoolName: 'Grand Mountain Charter School',
+        age: '28',
+        area: 'Ballingsmallard',
+        Salary: '$3300'
+    },
+    {
+        key: '8MFa3Drles',
+        img: 'https://i.postimg.cc/fRQw8bwv/teacher8.jpg',
+        name: 'Javier Lawson',
+        gender: 'Male',
+        position: 'Jr. Teacher',
+        schoolName: 'Bear Valley Technical School',
+        age: '25',
+        area: 'Chesterfield',
+        Salary: '$3000'
+    },
+    {
+        key: 'Zx8buOWZ5V',
+        img: 'https://i.postimg.cc/0NdrYwDm/teacher2.jpg',
+        name: 'Angela Hopes',
+        gender: 'Female',
+        position: 'Sr. Teacher',
+        schoolName: 'Grand Mountain Charter School',
+        age: '27',
+        area: 'Brahmanbaria',
+        Salary: '$3600'
+    },
+    {
+        key: 'Zx8bu0Wy5V',
+        img: 'https://i.postimg.cc/5NC45zM0/teacher10.jpg',
+        name: 'Mike Brandon',
+        gender: 'Male',
+        position: 'Principle Teacher',
+        schoolName: 'Iqra Chartered School',
+        age: '39',
+        area: 'Dhaka',
+        Salary: '$4100'
+    },
+]
+console.log(teachers);
