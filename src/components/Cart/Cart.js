43efd1ae4913810157066ed66661ca7e8989912e
salahuddinemon <emonthex@gmail.com>
2021-09-26 17:34:26 +0600

    comit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,14 +6,20 @@ const Cart = (props) => {
     let total = 0;
     let personName = [];
     for (const teacher of person) {
-        total = total + parseFloat(teacher.Salary)
-        personName = personName + teacher.name
+        total = total + parseFloat(teacher.Salary);
+        // personName = personName + teacher.name
+        personName.push(teacher.name)
     }
     return (
         <div>
             <h3>Added Teacher: {props.person.length}</h3>
             <h5>Total Salary: ${total}</h5>
-            <h5 className='display-selectName'>Name Of Added Teacher's:{personName} </h5>
+            <h5>Name of Added Teacher's: </h5>
+            <ol>
+                {personName.map((name) => (
+                    <li>{name}</li>
+                ))}
+            </ol>
         </div>
     );
 };
